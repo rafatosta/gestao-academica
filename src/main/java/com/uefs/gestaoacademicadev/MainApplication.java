4cbcb680b1d3e70d3a35d596e867f220f537dbe2
Rafael Tosta <rafa.ecomp@gmail.com>
2023-03-08 12:31:43 -0300

    :recycle: refactor: Refatora os nomes referente à Janela principal

--- src/main/java/com/uefs/gestaoacademicadev/MainApplication.java
+++ src/main/java/com/uefs/gestaoacademicadev/MainApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- src/main/java/com/uefs/gestaoacademicadev/controller/MainController.java
+++ src/main/java/com/uefs/gestaoacademicadev/controller/MainController.java
@@ -3,7 +3,7 @@ package com.uefs.gestaoacademicadev.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 

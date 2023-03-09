2284750368a2492b169e691d5f3529b17f03aa66
Rafael Tosta <rafa.ecomp@gmail.com>
2023-03-09 17:53:38 -0300

    :sparkles: feat: Criação da classe abstrata Pessoa

--- src/main/java/com/uefs/gestaoacademica/model/Pessoa.java
+++ src/main/java/com/uefs/gestaoacademica/model/Pessoa.java
@@ -1,4 +1,35 @@
 package com.uefs.gestaoacademica.model;
 
-public class Pessoa {
+public abstract class Pessoa {
+
+    private int id;
+
+    private String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public abstract String getMatricula();
+
+    @Override
+    public String toString() {
+        return this.id + "\t" + this.nome;
+    }
 }

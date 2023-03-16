799a2a535769667409c5e940bc616ee74051214d
Rafael Tosta <rafa.ecomp@gmail.com>
2023-03-16 08:51:50 -0300

    :sparkles: feat: Inscrições por aluno

--- src/main/java/com/uefs/gestaoacademica/model/Aluno.java
+++ src/main/java/com/uefs/gestaoacademica/model/Aluno.java
@@ -1,9 +1,16 @@
 package com.uefs.gestaoacademica.model;
 
+import com.uefs.gestaoacademica.dao.DAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno extends Pessoa {
+
 
     public Aluno(String nome) {
         super(nome);
+
     }
 
     @Override
@@ -11,5 +18,21 @@ public class Aluno extends Pessoa {
         return this.getId() + "AL";
     }
 
+    public List<Inscricao> getInscricoes() {
+        return DAO.getInscricao().getIncricoesByAluno(this);
+    }
 
+    public List<Inscricao> getInscricoes(int semestre) {
+        return DAO.getInscricao().getIncricoesByAluno(this, semestre);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Aluno aluno = (Aluno) obj;
+
+        if (this.getId() == aluno.getId()) {
+            return true;
+        }
+        return false;
+    }
 }
